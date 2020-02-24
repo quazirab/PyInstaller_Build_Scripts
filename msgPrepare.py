2eb73882b043c4d708251f7b503dac7e7906a849
quazirab <quazi.rabbi@gmail.com>
2020-02-24 15:11:36 -0800

    msgPrepare Updated

---
+++ msgPrepare.py
@@ -0,0 +1,40 @@
+'''
+Authur : Quazi Rabbi
+Objective : read current version number from __init__.py and add it to the git message
+How it works :  1. read the git message from .git/COMMIT_EDITMSG
+                2. read the __version__ in __init__.py
+                3. combine the msg with version at the front, msg cut if more than 72 characters.
+
+Additional Step - to run as git pre-commit hook - add a file 'pre-commit' (no file extension) in .git->hooks with the following lines :
+                #!/bin/sh
+                # To enable this hook, rename this file to "pre-commit".
+
+                python3 PyInstaller_Build_Scripts\\msgPrepare.py
+'''
+import sys,os,re,subprocess
+
+def getVersion(moduleName=None):
+    '''
+    Returns the version string from the __init__.py
+    '''
+    if not moduleName:
+        moduleName = os.getcwd().split('\\')[-1]
+    sys.path.append('.')
+    ver = __import__(f'{moduleName}').__version__
+    return (ver)
+
+def updateCommitMessage(version):
+    os.chdir('./.git')
+    line = None
+    with open('COMMIT_EDITMSG','r') as file:
+        line = file.read()
+    line = f'v{version}-{line}'
+
+    line=(line[:70]+'..') if len(line)>72 else line
+
+    with open('COMMIT_EDITMSG','w') as file:
+        file.write(line) 
+
+if __name__ == "__main__":
+    # print(getVersion())
+    updateCommitMessage(getVersion())
--- versionUpdate.py
+++ versionUpdate.py
@@ -4,7 +4,7 @@ Objective : update the commit number in __init__.py -> __version__ = 'Major.Mino
 How it works :  1. runs git rev-list command to get commit number
                 2. find the module name, which must should be same as main directory name .../documents/myModule/myModule/__init__.py
                     if the module name is not same as main directory name - then run python versionUpdate.py myModule
-                3. reads the __version__.py in __init__.py and replaces only the CommitNumber
+                3. reads the __version__ in __init__.py and replaces only the CommitNumber
 
 Additional Step - to run as git pre-commit hook - add a file 'pre-commit' (no file extension) in .git->hooks with the following lines :
                 #!/bin/sh
